9875967e2e2a73fd22e149a4d5b6bebfbe8d45b6
Filip Matejko <matejkofilip@gmail.com>
2023-06-29 21:45:40 +0200

    Black formatted.

--- src/RePiCore/InputLayer/base.py
+++ src/RePiCore/InputLayer/base.py
@@ -2,33 +2,34 @@ from typing import Union, List, Dict
 
 BaseClasses = Union[str, float, int, bool]
 
-class Source:
 
+class Source:
     def __init__(self) -> None:
         """
         Do not use. Only overwrite.
         """
-        raise NotImplementedError(f'Method __init__ not implemented in {self.__class__.__name__}')
+        raise NotImplementedError(
+            f"Method __init__ not implemented in {self.__class__.__name__}"
+        )
 
     def read(self) -> None:
         """
         Do not use. Only overwrite.
         """
-        raise NotImplementedError(f'Method read not implemented in {self.__class__.__name__}')
+        raise NotImplementedError(
+            f"Method read not implemented in {self.__class__.__name__}"
+        )
 
 
 class SingleValues(Source):
     __base_classes__ = (str, float, int, bool)
 
-    def __init__(
-        self,
-        **dictionary
-    ) -> None:
+    def __init__(self, **dictionary) -> None:
         # validate attribute
         assert isinstance(dictionary, dict)
         for k, v in dictionary.items():
             assert isinstance(k, str)
-            assert k != ''
+            assert k != ""
             if isinstance(v, list):
                 assert all([isinstance(i, self.__base_classes__) for i in v])
             else:
@@ -40,7 +41,7 @@ class SingleValues(Source):
     def read(self):
         for k, v in self.dictionary.items():
             self.__setattr__(k, v)
-        self.__delattr__('dictionary')
+        self.__delattr__("dictionary")
 
     def get_attributes(self) -> Dict[str, Union[BaseClasses, List[BaseClasses]]]:
         attributes = self.__dict__
--- tests/ut/RePiCore/InputLayer/test_SingleValues_class.py
+++ tests/ut/RePiCore/InputLayer/test_SingleValues_class.py
@@ -3,12 +3,20 @@ from RePiCore.InputLayer.base import SingleValues
 
 
 @pytest.mark.parametrize(
-    'd',
+    "d",
     [
-        ({'key': 1, 'k': 2, 'e': 1.1, 'y': True, 'test': [True, 'string', 1, 0, 1.111]}),
+        (
+            {
+                "key": 1,
+                "k": 2,
+                "e": 1.1,
+                "y": True,
+                "test": [True, "string", 1, 0, 1.111],
+            }
+        ),
         ({}),
-        ({'empty list': []})
-    ]
+        ({"empty list": []}),
+    ],
 )
 def test_init(d):
     SingleValues(**d)

a776f31ce20daceed859ebc2f9bf4814b66e1fa3
Filip Matejko <matejkofilip@gmail.com>
2023-07-04 21:13:03 +0200

    Adding guts to classes. Adjusting tests.

--- src/RePiCore/InputLayer/base.py
+++ src/RePiCore/InputLayer/base.py
@@ -1,4 +1,6 @@
-from typing import Union, List, Dict, Literal
+from typing import Union, List, Dict, Literal, Optional, Any
+import re
+import pandas as pd
 
 BaseClasses = Union[str, float, int, bool]
 
@@ -71,31 +73,58 @@ class FromDataBase(TableLike):
 
 class FromFile(TableLike):
     def __init__(self, path: str):
-        pass
+        self.dataframe: Optional[pd.DataFrame] = None
+        assert isinstance(path, str)
+        assert path != ""
+        self.path = path
 
     def read(self) -> None:
         super().read()
 
 
-class FromJson(FromFile):
-    def __init__(self, path: str, per_line_regex: str, headers: List[str]):
-        super().__init__(path)
-
-    def read(self) -> None:
-        pass
-
-
 class FromCsv(FromFile):
-    def __init__(self, path: str, delimiter: str):
+    def __init__(self, path: str, delimiter: str, options: Dict[str, Any]):
         super().__init__(path)
+        assert isinstance(delimiter, str)
+        assert len(delimiter) != 1
+        self.delimiter = delimiter
+        self.options = options
 
     def read(self) -> None:
-        pass
+        self.dataframe = pd.read_csv(
+            filepath_or_buffer=self.path, sep=self.delimiter, **self.options
+        )
 
 
 class FromExcel(FromFile):
-    def __init__(self, path: str, sheets: List[str]):
+    def __init__(self, path: str, sheet: Optional[str] = None):
         super().__init__(path)
+        assert isinstance(sheet, str) or (sheet is None)
+        self.sheet = sheet
 
     def read(self) -> None:
-        pass
+        if self.sheet is None:
+            self.dataframe = pd.read_excel(self.path)
+        else:
+            self.dataframe = pd.read_excel(self.path, sheet_name=self.sheet)
+
+
+class FromJson(FromFile):
+    def __init__(self, path: str, per_line_regex: str, headers: List[str]):
+        super().__init__(path)
+        assert isinstance(per_line_regex, str)
+        assert isinstance(headers, list)
+        assert all([isinstance(h, str) for h in headers])
+        self.per_line_regex = per_line_regex
+        self.headers = headers
+
+    def read(self) -> None:
+        with open(self.path, "r") as file:
+            lines = file.readlines()
+
+        groups = [re.findall(self.per_line_regex, line) for line in lines]
+        groups = [g.groups() for g in groups]  # type: ignore [attr-defined]
+
+        assert all([len(g) == len(self.headers) for g in groups])
+
+        self.dataframe = pd.DataFrame(data=groups, columns=self.headers)

6d5069a39643200ba94b8218d2ca905d67f63550
Filip Matejko <matejkofilip@gmail.com>
2023-07-05 18:26:11 +0200

    Improving coverage.

--- tests/ut/RePiCore/InputLayer/test_SingleValues_class.py
+++ tests/ut/RePiCore/InputLayer/test_SingleValues_class.py
@@ -1,14 +1,38 @@
 import pytest
 from RePiCore.InputLayer.base import SingleValues
 
-
-@pytest.mark.parametrize(
-    "d",
-    [
+test_cases = {
+    'arguments': 'd',
+    'cases': [
         ({"key": 1, "k": 2, "e": 1.1, "y": True, "test": [True, "string", 1, 0, 1.111]}),
         ({}),
         ({"empty list": []}),
-    ],
+    ]
+}
+
+
+@pytest.mark.parametrize(
+    test_cases['arguments'],
+    test_cases['cases']
 )
 def test_init(d):
     SingleValues(**d)
+
+
+@pytest.mark.parametrize(
+    test_cases['arguments'],
+    test_cases['cases']
+)
+def test_read(d):
+    o = SingleValues(**d)
+    o.read()
+
+
+@pytest.mark.parametrize(
+    test_cases['arguments'],
+    test_cases['cases']
+)
+def test_get_attributes(d):
+    o = SingleValues(**d)
+    o.read()
+    o.get_attributes()

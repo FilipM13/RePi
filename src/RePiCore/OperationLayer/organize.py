b2a798ae20e41e41826896f6f4f2abc619c639f6
Filip Matejko <matejkofilip@gmail.com>
2023-07-19 20:32:00 +0200

    Adding guts to Line class. Changes to Operation classes might be required.

--- src/RePiCore/OperationLayer/organize.py
+++ src/RePiCore/OperationLayer/organize.py
@@ -5,7 +5,24 @@ from .base import Operation
 
 class Line:
     def __init__(self, operations: List[Union[Operation, "Line"]]) -> None:
-        pass
+        assert isinstance(operations, list)
+        assert all([isinstance(op, (Operation, Line)) for op in operations])
+        self.__inputs__ = operations[0].__inputs__  # type: ignore [union-attr,has-type]
+        self.__outputs__ = operations[-1].__outputs__  # type: ignore [union-attr,has-type]
+        for i in range(len(operations) - 1):
+            assert operations[i].__inputs__ == operations[i + 1].__inputs__  # type: ignore [union-attr]
+            assert operations[i].__outputs__ == operations[i + 1].__outputs__  # type: ignore [union-attr]
+        self.operations = operations
 
-    def execute(self) -> Any:
-        pass
+    def execute(self, *inputs: Any) -> Any:
+        for i, v in zip(inputs, self.__inputs__):
+            assert isinstance(i, v)
+
+        _tmp_ = inputs
+        for op in self.operations:
+            _tmp_ = op.execute(*_tmp_)
+
+        for t, v in zip(_tmp_, self.__outputs__):
+            assert isinstance(t, v)
+
+        return _tmp_
